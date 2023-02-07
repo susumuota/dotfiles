9739bf3c614e54dac1096dac3d2aef0b7b639b0a
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-02-08 00:17:31 +0900

    fix for latest version.

--- setup/install_docker.sh
+++ setup/install_docker.sh
@@ -3,7 +3,7 @@
 # https://docs.docker.com/engine/install/ubuntu/
 # https://docs.docker.com/engine/install/linux-postinstall/
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 sudo apt-get install -y \
@@ -19,7 +19,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # sudo docker run hello-world
 
